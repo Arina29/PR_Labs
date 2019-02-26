2bf0e9bef5a87aad2770307606a90916d0796af7
Arina Chirosca <arina.chirosca@gmail.com>
2019-02-26 22:23:37 +0200

    Lab2 added

---
+++ lab2/multiThreading/multiThreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace multiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => writeNode(4), () => writeNode(5));
+            Task task2 = Task.Factory.StartNew(() => writeNode(2));
+            Task.WaitAll(task2);
+            Task task3 = Task.Factory.StartNew(() => writeNode(3));
+            Task.WaitAll(task2, task3);
+            Task task1 = Task.Factory.StartNew(() => writeNode(1));
+
+            Console.ReadKey();
+        }
+
+        static void writeNode(object nodeCode) 
+        {
+            Console.WriteLine("Thread" + (int)nodeCode);
+            Thread.Sleep(1500);
+        }
+        
+    }
+}
